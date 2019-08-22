ea1923fa1b6ca4276c8bcfedf5c2c905e961e7ad
A Pottinger <samp@gleap.org>
2019-08-22 15:55:09 -0700

    Fix typo with classifiers.

--- setup.py
+++ setup.py
@@ -32,20 +32,5 @@ setuptools.setup(
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Text Processing'
     ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Legal Industry',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Healthcare Industry',
-        'Intended Audience :: Telecommunications Industry',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Information Analysis'
-    ],
     download_url='https://github.com/sampottinger/wandb_summarizer/archive/master.zip'
 )
