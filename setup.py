27641628dd1ae171b71a1b90c26740c48270b027
A Pottinger <samp@gleap.org>
2019-08-22 15:54:15 -0700

    Include additional fields in setup.

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setuptools.setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='',
-    author='Sam Pottinger',
-    author_email='sam@datadrivenempathy.com',
+    author='A. Samuel Pottinger',
     license='MIT',
     packages=['wandb_summarizer'],
     install_requires=[
@@ -16,5 +15,37 @@ setuptools.setup(
     ],
     entry_points = {
         'console_scripts': ['wandb-summarizer-to-csv=wandb_summarizer.export:main'],
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+    ],
+    download_url='https://github.com/sampottinger/wandb_summarizer/archive/master.zip'
 )
